9b159aad77f7179675729f35d7a42ab644d5b489
zx <zx@192.168.136.1>
2020-02-05 20:12:28 +0800

    程序计数器使用例子

---
+++ demo1/src/com/xuge/demo1/ProgramCounterDemo.java
@@ -0,0 +1,19 @@
+package com.xuge.demo1;
+
+public class ProgramCounterDemo {
+
+	public int add() {
+		int a = 100;
+		int b = 200;
+		int c = 300;
+		return (a+b)*c;
+	}
+	
+	public void test1() {
+		System.currentTimeMillis();
+	}
+	
+	public static int test2(int a,  int b) {
+		return a+b;
+	}
+}
